709c9c50ec2b9b0f0047e2416222c32cb79ac4b2
Christopher Aubuchon <caubuchon@petrichor.io>
2022-02-16 13:22:36 -0800

    updated

--- script.sh
+++ script.sh
@@ -3,3 +3,7 @@
 git push
 
 
+curl https://api.cycle.io/v1/pipelines/620d6a31ce1c32daad5e0f94/trigger \
+  -H "Content-Type: application/json" \
+  -d '{ "secret": "secret_USFzH4E1mEBxqxhzIl9simZGoZMgaf4kf3s4MNSNzrEyEZuEmmfGRqmhJJX3" }' \
+  -X POST
